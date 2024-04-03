2d27e7bd21cb0d2f2e90bc05d9e78c453c89b818
Your Name <your.email@example.com>
2024-04-03 19:22:32 +0300

    testing - prakt. darbas 3

--- app/src/test/java/com/example/praktikosdarbas2/ExampleUnitTest.java
+++ app/src/test/java/com/example/praktikosdarbas2/ExampleUnitTest.java
@@ -11,7 +11,64 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Given_EmptyText_getCharsCount_ZeroReturned() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_EmptyText_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_NormalText_getCharsCount_CorrectValueReturned() {
+        String givenString = "Laba diena";
+        int expectedValue = 10;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_NormalText_getWordsCount_CorrectValueReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "Laba diena";
+        int expectedValue = 2;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_OnlySpaces_getCharsCount_CorrectValueReturned() {
+        String givenString = "     "; // 5 spaces
+        int expectedValue = 5;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void Given_OnlySpaces_getWordsCount_ZeroReturned() {
+        TextCounter tc = new TextCounter();
+        String givenString = "     "; // 5 spaces
+        int expectedValue = 0;
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_SemicolonsAndSpaces_getCharsCount_CorrectValueReturned() {
+        String givenString = ";; : ;;;;";
+        int expectedValue = 9;
+        int actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void Given_SemicolonsAndSpaces_getWordsCount_ZeroReturned() {
+        String givenString = ";; : ;;;;";
+        int expectedValue = 0;
+        TextCounter tc = new TextCounter();
+        int actualValue = tc.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
     }
 }
